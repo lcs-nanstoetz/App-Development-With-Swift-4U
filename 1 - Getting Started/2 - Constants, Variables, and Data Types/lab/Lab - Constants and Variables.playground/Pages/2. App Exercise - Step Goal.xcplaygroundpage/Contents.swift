c12341a895a0429ae7de4b94c8f7cc4166b9e062
Nick Anstoetz <nanstoetz@lcs.on.ca>
2019-10-03 20:55:40 -0400

    finished it up

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/2. App Exercise - Step Goal.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/2. App Exercise - Step Goal.xcplaygroundpage/Contents.swift
@@ -10,6 +10,8 @@
 /*:
  Use two `print` functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of `goalSteps` by referencing your constant.
  */
-
+let goalStep = 10000
 
 //: [Previous](@previous)  |  page 2 of 10  |  [Next: Exercise - Variables](@next)
+print("your stepGoal for today is:")
+print(goalStep)
