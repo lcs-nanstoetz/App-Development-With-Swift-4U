53a983253c4f05cff32366381622d2da90c89a3a
Nick Anstoetz <nanstoetz@lcs.on.ca>
2019-10-09 14:09:22 -0400

    finished  4.3

--- 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
@@ -8,14 +8,20 @@
 let stepGoal = 10000
 let step = 1000
 if stepGoal / 2 > step {
-    
+    print("You're almost halfway there!")
 } else {
-    print(<#T##items: Any...##Any#>)
+    print("You're over halfway there!" )
 }
 
 /*:
  Now create a new, but similar, if-else-if statement that prints "Way to get a good start today!" if `steps` is less than a tenth of `stepGoal`, prints "You're almost halfway there!" if `steps` is less than half of `stepGoal`, and prints "You're over halfway there!" if `steps` is greater than half of `stepGoal`.
  */
-
+if stepGoal / 10 >= step {
+    print("Way to get a good start today!")
+} else if stepGoal / 2 > step {
+    print("You're almost halfway there!")
+} else {
+    print("You're over halfway there!")
+}
 
 //: [Previous](@previous)  |  page 3 of 9  |  [Next: Exercise - Boolean Practice](@next)
