1da1c94109e3f330cd1baf365a0b441ffae2ff23
Nick Anstoetz <nanstoetz@lcs.on.ca>
2019-10-09 16:13:46 -0400

    finished 4.7finished 4.8

--- 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ var largest: Int
 if number1 > number2 {
     largest = number1
 } else {
-    largest = number2
+    largest = number1 > number2 ? number1 : number2
 }
 
 //: [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
