fda5c91b6c17d584063e1cb85bfa6525f439fe6d
Nick Anstoetz <nanstoetz@lcs.on.ca>
2019-10-09 13:05:38 -0400

    finished 2.5

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/5. Exercise - Constant Or Variable.xcplaygroundpage/Contents.swift
@@ -10,9 +10,9 @@
  
  For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
  */
-
-
-
-
-
+var NumberOfLikes: Int
+var NumberOfComments: Int
+let YearCreated: Int
+let MonthCreated: Int
+let DayCreated: Int
 //: [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Fitness Tracker: Constant or Variable?](@next)
